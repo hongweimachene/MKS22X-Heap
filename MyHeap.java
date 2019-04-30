5b7d62655399cd37d720ff87e23977f4c2e433c9
hongweimachene <hchen00@stuy.edu>
2019-04-30 11:48:16 -0400

    worked on pushdown

--- MyHeap.java
+++ MyHeap.java
@@ -1,14 +1,22 @@
 public class MyHeap{
   private static void pushDown(int[]data,int size,int index){
-    
+    int value = data[index];
+    int child1 = data[2*index+1];
+    int child2 = data[2*index+2];
+    while ((2*index+ 1 < size && 2*index+2 < size)
+       if (value < data[2*index+1]){
+
+       }
+
+    }
   }
   private static void pushUp(int[]data,int index){
-    
+
   }
-  public static void heapify(int[]){
-    
+  public static void heapify(int[] data){
+
   }
-  public static void heapsort(int[]){
-    
+  public static void heapsort(int[] data){
+
   }
 }
