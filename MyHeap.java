3972d025ec3c10db46b7fd72f80bae1a28fcc62f
hongweimachene <hchen00@stuy.edu>
2019-05-01 00:46:20 -0400

    finished pushdown

--- MyHeap.java
+++ MyHeap.java
@@ -1,6 +1,11 @@
 public class MyHeap{
+  /*- size  is the number of elements in the data array.
+     - push the element at index i downward into the correct position. This will swap with the larger of the child nodes provided that child is larger. This stops when a leaf is reached, or neither child is larger. [ should be O(logn) ]
+     - precondition: index is between 0 and size-1 inclusive
+     - precondition: size is between 0 and data.length-1 inclusive
+  */
   private static void pushDown(int[]data,int size,int index){
-    boolean hasChildren = true;
+    boolean hasChildren = true; //if the parent at index has any children to do valid swaps with, assume true
     while (hasChildren) {
       int ch1 = 2*index+1;
       int ch2 = 2*index+2;
@@ -15,11 +20,20 @@ public class MyHeap{
         } else {
           hasChildren = false;
         }
-    } else if (ch1 < size && ch2 < size) {
-      if (Math.max(data[ch1], data[ch2]) == data[ch1] && data[index] < data[ch1]){
-        int replace = data[index];
-        data[index] = data[ch1];
-        data[ch1];
+      } else if (ch1 < size && ch2 < size) {
+        if (data[index] >= data[ch1] && data[index] >= data[ch2]){
+          hasChildren = false;
+        } else if (Math.max(data[ch1], data[ch2]) == data[ch1] && data[index] < data[ch1]){
+          int replace = data[index];
+          data[index] = data[ch1];
+          data[ch1] = replace;
+          index = ch1;
+        } else if (Math.max(data[ch1],data[ch2]) == data[ch2] && data[index] < data[ch2]){
+          int replace = data[index];
+          data[index] = data[ch2];
+          data[ch2] = replace;
+          index = ch2;
+        }
       }
     }
   }
