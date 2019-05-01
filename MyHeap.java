5228b0124eba2fe4a3c7924d1d1f1d0ace09e528
hongweimachene <hchen00@stuy.edu>
2019-05-01 00:37:15 -0400

    working on pushdown

--- MyHeap.java
+++ MyHeap.java
@@ -1,13 +1,26 @@
 public class MyHeap{
   private static void pushDown(int[]data,int size,int index){
-    int value = data[index];
-    int child1 = data[2*index+1];
-    int child2 = data[2*index+2];
-    while ((2*index+ 1 < size && 2*index+2 < size)
-       if (value < data[2*index+1]){
-
-       }
-
+    boolean hasChildren = true;
+    while (hasChildren) {
+      int ch1 = 2*index+1;
+      int ch2 = 2*index+2;
+      if (ch1 >= size && ch2 >= size){
+        hasChildren = false;
+      } else if (ch1 < size && ch2 >= size) {
+        if (data[index] < data[ch1]){
+          int replace = data[index];
+          data[index] = data[ch1];
+          data[ch1] = replace;
+          index = ch1;
+        } else {
+          hasChildren = false;
+        }
+    } else if (ch1 < size && ch2 < size) {
+      if (Math.max(data[ch1], data[ch2]) == data[ch1] && data[index] < data[ch1]){
+        int replace = data[index];
+        data[index] = data[ch1];
+        data[ch1];
+      }
     }
   }
   private static void pushUp(int[]data,int index){
